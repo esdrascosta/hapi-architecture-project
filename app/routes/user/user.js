722271a8b6f3f5e016701893dfce3d3e3077c253
Esdras Costa <servoesdras@gmail.com>
2015-09-22 19:48:52 -0300

    remove folder user

--- app/routes/user/user.js
+++
@@ -1,9 +0,0 @@
-var User = require('../../controllers/userController')
-
-module.exports = [
-  { method: 'POST', path: '/user', handler: User.create},
-  { method: 'GET', path: '/user', handler: User.getAll},
-  { method: 'GET', path: '/user/{userId}', handler: User.getOne},
-  { method: 'PUT', path: '/user/{userId}', handler: User.update},
-  { method: 'DELETE', path: '/user/{userId}', handler: User.remove},
-]
